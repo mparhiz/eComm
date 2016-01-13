b3f162fa4bcad7488ef0afd66174b51438127e29
Mohsen Parhiz <m_parhiz@yahoo.com>
2016-01-13 16:35:26 +1100

    new version

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -8,11 +8,23 @@
   /** @ngInject */
   function routerConfig($stateProvider, $urlRouterProvider) {
     $stateProvider
+    /*  
       .state('home', {
         url: '/',
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+*/
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/homepage/main.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .state('home.products', {
+        url: '/products',
+        templateUrl: 'app/homepage/products.html'
       });
 
     $urlRouterProvider.otherwise('/');
